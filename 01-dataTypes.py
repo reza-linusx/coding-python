dc415f8fa4c8c1c80f71480366b62f5462713fe6
Reza <reza.linusx@gmail.com>
2025-09-02 15:44:01 +0330

    Add examples of basic data types in python

---
+++ 01-dataTypes.py
@@ -0,0 +1,27 @@
+# Python Data Types Examples
+
+# Integer: whole numbers
+integ = 44
+
+# Float: numbers with decimals
+flt = 3.14
+
+# String: text enclosed in quotes
+my_str = 'this is my string'
+
+# List: ordered collection of items, can be mixed types, mutable
+lis = ['pizza', 3.22, 60, False, True]
+
+# Dictionary: key-value pairs, unordered, mutable
+dic = {
+    'name': 'reza',
+    'pi': 3.14,
+    'age': 26
+}
+
+# Tuple: ordered collection, immutable (cannot be changed after creation)
+tup = ("something", True, 33)
+
+# Boolean: True or False values
+correct = True
+incorrect = False
